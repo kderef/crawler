6f42e0cb30a88de15cb89dfa1b4baeba63e4a945
kvoid <kianheitkamp@outlook.com>
2025-08-08 21:55:18 +0200

    reset

--- src/text.rs
+++
@@ -1,9 +0,0 @@
-use std::ops::IndexMut;
-
-pub trait DrawText {
-    fn draw_text(&mut self, text: impl AsRef<str>, x: u32, y: u32, size: u32) {}
-}
-
-impl DrawText for Vec<u32> {
-    fn draw_text(&mut self, text: impl AsRef<str>, x: u32, y: u32, size: u32) {}
-}
