5cd1b0908d3bfdd8faf04b39d75af3418c4768eb
kderef <kianheitkamp@outlook.com>
2025-08-31 19:57:21 +0200

    C++ test

---
+++ cpm.h
@@ -0,0 +1,16 @@
+//--------------------------
+// cpm.h - C plus minus
+//--------------------------
+
+/*************************************/
+// === RULES === //
+
+// 1. NO CONSTRUCTORS/DESTRUCTORS, use factories
+// 2. NO C++ STANDARD LIBRARY, use libc instead.
+// 3. NO CLASSES/INHERITANCE ALLOWED, use structs
+
+
+namespace cpm {
+
+    
+}
