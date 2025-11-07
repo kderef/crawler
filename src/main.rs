5274d71fbbe4b10a38c849f8bba6c0238754f7ef
kvoid <kianheitkamp@outlook.com>
2025-11-07 11:37:13 +0100

    back to macroquad ig

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+mod terminal;
+
 use macroquad::prelude::*;
 
-use crate::camera::CameraController;
-
-mod camera;
+/*
+- implement Terminal struct
+*/
 
 fn conf() -> Conf {
     Conf {
@@ -17,26 +19,8 @@ fn conf() -> Conf {
 
 #[macroquad::main(conf)]
 async fn main() {
-    let mut camera = CameraController::new();
-    let mut cam_active = true;
-
-    camera.set_active(cam_active);
-
     loop {
-        if is_key_pressed(KeyCode::Tab) {
-            cam_active ^= true;
-            camera.set_active(cam_active);
-        }
-
-        camera.look();
-
-        set_camera(&camera);
-        {
-            draw_cube_wires(vec3(10., 10., 10.), vec3(10., 10., 10.), RED);
-        }
-        set_default_camera();
-
-        draw_text("hello!", 50., 50., 30., WHITE);
+        draw_text("hallo!", 50., 50., 30., WHITE);
         next_frame().await;
     }
 }
