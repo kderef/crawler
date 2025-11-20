8f0e9bc7b46a26661760136df7e85dd241718995
kvoid <kianheitkamp@outlook.com>
2025-11-20 13:13:24 +0100

    sdl2

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,52 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-extern crate sdl3;
-
-use sdl3::event::Event;
-use sdl3::keyboard::Keycode;
-use sdl3::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::messagebox::{MessageBoxFlag, show_message_box, show_simple_message_box};
+use sdl2::pixels::Color;
+use sdl2::render::WindowCanvas;
+use sdl2::{Sdl, VideoSubsystem};
 use std::time::Duration;
 
-fn main() {
-    run().unwrap();
+pub struct App {
+    sdl: Sdl,
+    video: VideoSubsystem,
+    canvas: WindowCanvas,
 }
 
-fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let sdl_context = sdl3::init()?;
-    let video_subsystem = sdl_context.video()?;
-
-    let window = video_subsystem
-        .window("rust-sdl3 demo: Video", 800, 600)
+fn main() {
+    let sdl = sdl2::init().unwrap();
+    let video = sdl.video().unwrap();
+    let mut window = video
+        .window("crawler", 800, 600)
         .position_centered()
+        .resizable()
         .opengl()
+        .allow_highdpi()
         .build()
-        .map_err(|e| e.to_string())?;
+        .unwrap();
 
-    let mut canvas = window.into_canvas();
+    window.raise();
+
+    let canvas = window.into_canvas().build().unwrap();
+
+    let mut app = App { sdl, video, canvas };
+
+    if let Err(e) = run(&mut app) {
+        let flags = MessageBoxFlag::ERROR;
+        let title = "ERROR";
+        let message = &e;
+        show_simple_message_box(flags, title, message, app.canvas.window()).unwrap();
+    }
+}
+
+pub fn run(app: &mut App) -> Result<(), String> {
+    let App { canvas, sdl, video } = app;
 
     canvas.set_draw_color(Color::RGB(255, 0, 0));
     canvas.clear();
     canvas.present();
-    let mut event_pump = sdl_context.event_pump()?;
+    let mut event_pump = sdl.event_pump()?;
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -43,7 +62,7 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
 
         canvas.clear();
         canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
         // The rest of the game loop goes here...
     }
 
