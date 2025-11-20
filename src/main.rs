96df387e6d4d60479132ce9d170636bdf3052337
kvoid <kianheitkamp@outlook.com>
2025-11-20 12:13:07 +0100

    sdl3

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+extern crate sdl3;
+
+use sdl3::event::Event;
+use sdl3::keyboard::Keycode;
+use sdl3::pixels::Color;
+use std::time::Duration;
+
+fn main() {
+    run().unwrap();
+}
+
+fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let sdl_context = sdl3::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem
+        .window("rust-sdl3 demo: Video", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window.into_canvas();
+
+    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump()?;
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        canvas.clear();
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
+        // The rest of the game loop goes here...
+    }
+
+    Ok(())
+}
